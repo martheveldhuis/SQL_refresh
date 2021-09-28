4a25d6eb5db8fd0ecfe12dc8e75e9539779c4723
martheveldhuis <msveldhuis96@gmail.com>
2021-09-28 12:55:56 +0200

    Uncomment exercises.

--- exercises/stored_procedures.sql
+++ exercises/stored_procedures.sql
@@ -1,200 +1,200 @@
 -- Create stored procedure
--- DELIMITER $$
--- CREATE PROCEDURE get_clients()
--- BEGIN
--- 	SELECT * FROM clients;
--- END$$
+DELIMITER $$
+CREATE PROCEDURE get_clients()
+BEGIN
+	SELECT * FROM clients;
+END$$
 
--- DELIMITER ;
+DELIMITER ;
 
 -- Call it.
--- CALL get_clients();
+CALL get_clients();
 
 -- Create a stored procedure called get_invoices_with_balance
 -- to return all invoices with a balance > 0.
 
--- DELIMITER $$
--- CREATE PROCEDURE get_invoices_with_balance()
--- BEGIN
--- 	SELECT * 
---     FROM invoices_with_balance
---     WHERE balance > 0;
--- END$$
+DELIMITER $$
+CREATE PROCEDURE get_invoices_with_balance()
+BEGIN
+	SELECT * 
+    FROM invoices_with_balance
+    WHERE balance > 0;
+END$$
 
--- DELIMITER ;
+DELIMITER ;
 
 -- Standard formula for a stored procedure
 
--- DROP PROCEDURE IF EXISTS get_clients;
+DROP PROCEDURE IF EXISTS get_clients;
 
--- DELIMITER $$
--- CREATE PROCEDURE get_clients()
--- BEGIN
--- 	SELECT * FROM clients;
--- END$$
+DELIMITER $$
+CREATE PROCEDURE get_clients()
+BEGIN
+	SELECT * FROM clients;
+END$$
 
--- DELIMITER ;
+DELIMITER ;
 
 -- Add parameters.
 
--- DROP PROCEDURE IF EXISTS get_clients_by_state;
+DROP PROCEDURE IF EXISTS get_clients_by_state;
 
--- DELIMITER $$
--- CREATE PROCEDURE get_clients_by_state
--- (
--- 	state CHAR(2)
--- )
--- BEGIN
--- 	SELECT * FROM clients c
---     WHERE c.state = state;
--- END$$
+DELIMITER $$
+CREATE PROCEDURE get_clients_by_state
+(
+	state CHAR(2)
+)
+BEGIN
+	SELECT * FROM clients c
+    WHERE c.state = state;
+END$$
 
--- DELIMITER ;
+DELIMITER ;
 
--- CALL get_clients_by_state('CA');
+CALL get_clients_by_state('CA');
 
 -- Write a stored procedure to return invoices for a given client.
 
--- DROP PROCEDURE IF EXISTS get_invoices_by_client;
+DROP PROCEDURE IF EXISTS get_invoices_by_client;
 
--- DELIMITER $$
--- CREATE PROCEDURE `get_invoices_by_client`
--- (
--- 	client_id INT
--- )
--- BEGIN
--- 	SELECT *
---     FROM invoices i
---     WHERE i.client_id = client_id;
--- END$$
--- DELIMITER ;
+DELIMITER $$
+CREATE PROCEDURE `get_invoices_by_client`
+(
+	client_id INT
+)
+BEGIN
+	SELECT *
+    FROM invoices i
+    WHERE i.client_id = client_id;
+END$$
+DELIMITER ;
 
 -- Parameters with default values
 
--- DROP PROCEDURE IF EXISTS get_clients_by_state;
+DROP PROCEDURE IF EXISTS get_clients_by_state;
 
--- DELIMITER $$
--- CREATE PROCEDURE get_clients_by_state
--- (
--- 	state CHAR(2)
--- )
--- BEGIN
--- 	IF state IS NULL THEN
--- 		SET state = 'CA';
--- 	END IF;
---     
--- 	SELECT * FROM clients c
---     WHERE c.state = state;
--- END$$
+DELIMITER $$
+CREATE PROCEDURE get_clients_by_state
+(
+	state CHAR(2)
+)
+BEGIN
+	IF state IS NULL THEN
+		SET state = 'CA';
+	END IF;
+    
+	SELECT * FROM clients c
+    WHERE c.state = state;
+END$$
 
--- DELIMITER ;
+DELIMITER ;
 
 -- Return all clients.
 
--- DROP PROCEDURE IF EXISTS get_clients_by_state;
+DROP PROCEDURE IF EXISTS get_clients_by_state;
 
--- DELIMITER $$
--- CREATE PROCEDURE get_clients_by_state
--- (
--- 	state CHAR(2)
--- )
--- BEGIN
--- 	SELECT * FROM clients c
--- 	WHERE c.state = IFNULL(state, c.state);
--- END$$
+DELIMITER $$
+CREATE PROCEDURE get_clients_by_state
+(
+	state CHAR(2)
+)
+BEGIN
+	SELECT * FROM clients c
+	WHERE c.state = IFNULL(state, c.state);
+END$$
 
--- DELIMITER ;
+DELIMITER ;
 
 -- Write a stored procedure called get_payments with 2 params:
 -- client_id (INT), payment_method_id (TINYINT)
 
--- DROP PROCEDURE IF EXISTS get_payments;
+DROP PROCEDURE IF EXISTS get_payments;
 
--- DELIMITER $$
--- CREATE PROCEDURE get_payments
--- (
--- 	client_id INT,
---     payment_method_id TINYINT
--- )
--- BEGIN
--- 	SELECT * FROM payments p
--- 	WHERE 
--- 		p.client_id = IFNULL(client_id, p.client_id) AND 
---         p.payment_method = IFNULL(payment_method_id, p.payment_method);
--- END$$
+DELIMITER $$
+CREATE PROCEDURE get_payments
+(
+	client_id INT,
+    payment_method_id TINYINT
+)
+BEGIN
+	SELECT * FROM payments p
+	WHERE 
+		p.client_id = IFNULL(client_id, p.client_id) AND 
+        p.payment_method = IFNULL(payment_method_id, p.payment_method);
+END$$
 
--- DELIMITER ;
+DELIMITER ;
 
 -- Parameter Validation
 
--- DELIMITER $$
--- CREATE PROCEDURE make_payment
--- (
--- 	invoice_id INT,
---     payment_amount DECIMAL(9, 2),
---     payment_date DATE
--- )
--- BEGIN
--- 	IF payment_amount <= 0 THEN
--- 		SIGNAL SQLSTATE '22003' 
--- 			SET MESSAGE_TEXT = 'Invalid payment amount';
--- 	END IF;
--- 	UPDATE invoices i
---     SET 
--- 		i.payment_total = payment_amount,
--- 		i.payment_date = payment_date
---     WHERE i.invoice_id = invoice_id;
--- END$$
--- DELIMITER ;
+DELIMITER $$
+CREATE PROCEDURE make_payment
+(
+	invoice_id INT,
+    payment_amount DECIMAL(9, 2),
+    payment_date DATE
+)
+BEGIN
+	IF payment_amount <= 0 THEN
+		SIGNAL SQLSTATE '22003' 
+			SET MESSAGE_TEXT = 'Invalid payment amount';
+	END IF;
+	UPDATE invoices i
+    SET 
+		i.payment_total = payment_amount,
+		i.payment_date = payment_date
+    WHERE i.invoice_id = invoice_id;
+END$$
+DELIMITER ;
 
 -- Output parameters
 
--- DELIMITER $$
--- CREATE PROCEDURE get_unpaid_invoices_for_client
--- (
--- 	client_id INT,
---     OUT invoices_count INT,
---     OUT invoices_total DECIMAL(9, 2)
--- )
--- BEGIN
--- 	SELECT COUNT(*), SUM(invoice_total)
---     INTO invoices_count, invoices_total
---     FROM invoices i
---     WHERE i.client_id = client_id
---     AND payment_total = 0;
--- END$$
--- DELIMITER ;
+DELIMITER $$
+CREATE PROCEDURE get_unpaid_invoices_for_client
+(
+	client_id INT,
+    OUT invoices_count INT,
+    OUT invoices_total DECIMAL(9, 2)
+)
+BEGIN
+	SELECT COUNT(*), SUM(invoice_total)
+    INTO invoices_count, invoices_total
+    FROM invoices i
+    WHERE i.client_id = client_id
+    AND payment_total = 0;
+END$$
+DELIMITER ;
 
 -- User or session variables
 
--- SET @invoices_count = 0;
+SET @invoices_count = 0;
 
 -- Local variables
 
--- DELIMITER $$
--- CREATE PROCEDURE get_risk_factor()
--- BEGIN
--- 	DECLARE risk_factor DECIMAL(9, 2) DEFAULT 0;
---     DECLARE invoices_total DECIMAL(9, 2);
---     DECLARE invoices_count INT;
---     
---     SELECT COUNT(*), SUM(invoice_total)
---     INTO invoices_count, invoices_total
---     FROM invoices;
---     
---     SET risk_factor = invoices_total / invoices_count * 5;
---     
---     SELECT risk_factor;
--- END$$
--- DELIMITER ;
+DELIMITER $$
+CREATE PROCEDURE get_risk_factor()
+BEGIN
+	DECLARE risk_factor DECIMAL(9, 2) DEFAULT 0;
+    DECLARE invoices_total DECIMAL(9, 2);
+    DECLARE invoices_count INT;
+    
+    SELECT COUNT(*), SUM(invoice_total)
+    INTO invoices_count, invoices_total
+    FROM invoices;
+    
+    SET risk_factor = invoices_total / invoices_count * 5;
+    
+    SELECT risk_factor;
+END$$
+DELIMITER ;
 
 -- Functions
 -- Look at function_get_risk_factor_for_client.sql
 
--- SELECT
--- 	client_id,
---     name,
---     get_risk_factor_for_client(client_id) AS risk_factor
--- FROM clients;
+SELECT
+	client_id,
+    name,
+    get_risk_factor_for_client(client_id) AS risk_factor
+FROM clients;
 
 DROP FUNCTION IF EXISTS get_risk_factor_for_client;
